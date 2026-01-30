4cc4df1154c87a1af9d5a9d3f51eb22488623dcd
Ben-star-afk <mayegunlives@gmail.com>
2026-01-30 18:21:20 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import mysql.connector
+import re
 from flask import Flask, render_template, request
 app = Flask(__name__)
 @app.route('/login', methods=['GET','POST'])
@@ -38,10 +39,40 @@ def logout():
 @app.route('/')
 def home():
     return render_template('login.html')
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/register', methods=['GET','POST'])
 def register():
-    if request.method == 'POST':
-        return render_template('index.html')
-    return render_template('register.html')
+    msg=''
+    if request.method == 'POST' and username in request.form and password in request.form and email in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        mydb = mysql.connetcor.connect(
+            host = 'remoteysql.com',
+            user = 'Rz8hqnlk4',
+            password = 'nd6wK03xe0',
+            database= 'Rz8hqnlk4'   
+        )
+        mycursor = mydb.cursor()
+        print(username)
+        mycursor.execute('SELECT * FROM LoginDetails WHERE Name = %s AND Email_id = %s', (username, email))
+        account=mycursor.fetchone()
+        print(account)
+        if account:
+            msg = 'Account already exists!'
+        elif not re.match(r'^[^\d]*@[^\d]*\.\.[^\d]*$',email):
+            msg = 'Invalid email address!'
+        elif not re.match(r'(A-Za_z0-9)+',username):
+            msg = 'Username must contain only characters and numbers!'
+        elif not username or not password or not email:
+            msg='Kindly fill the details!'
+        else:
+            mycursor.execute('INSERT INTO LoginDetails VALUES (NULL, %s, %s, %s)',(username, password, email))
+            mydb.commit()
+            mdg = 'Your Registration is successful'
+            name = username
+            return render_template('index.html', msg=msg, name=name)
+    elif request.method == 'POST':
+        msg='Kindly fill the details!'
+        return render_template('registration.html', msg=msg)
 if __name__=='__main__':
     app.run(debug=True)
